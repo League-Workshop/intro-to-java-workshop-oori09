e3dd2a7e8dccd73ff7b239c9e00c0d1b7ca27d62
root <root@9adaa725f63c>
2020-04-15 22:00:25 +0000

    day 3

---
+++ src/section3/Greeter.java
@@ -0,0 +1,17 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	String name =  JOptionPane.showInputDialog("type in your name");
+	
+	JOptionPane.showMessageDialog(null, "hello " + name);
+	
+	if(name.equals("oori"));
+	JOptionPane.showMassegeDialog(null, "you are a good person");
+	else {
+JOptionPane.showMessageDialog(null, "hello " + name);	
+}
+}
+}
